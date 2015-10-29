8da5eab5161b3e97a12571f3c69da4d5a18f439f
rattanakone <rattanakone@users.noreply.github.com>
2015-10-28 18:40:22 -0700

    configure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openWeatherMapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the openWeatherMapApp
+ */
+angular.module('openWeatherMapApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openWeatherMapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openWeatherMapApp
+ */
+angular.module('openWeatherMapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
