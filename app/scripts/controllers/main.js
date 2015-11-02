fa1a2d34e9a8a274270003dbb7e72df6c2e80be6
rattanakone <rattanakone@users.noreply.github.com>
2015-11-01 19:55:30 -0800

    testing 2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,13 +25,3 @@ angular.module('openWeatherMapApp')
 
 
 
-//angular.module('openWeatherMapApp')
-//  .controller('MainCtrl', function ($scope, current) {
-//    $scope.current = current.query();
-
-//    $scope.refreshCurrent = function(){
-//        $scope.current = current.query({
-//            location: $scope.location
-//        });
-//    };
-//  });
