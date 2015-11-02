bba434b336735b463df3857f87872e35c67a504e
rattanakone <rattanakone@users.noreply.github.com>
2015-11-01 19:52:20 -0800

    testing

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -15,29 +15,5 @@ angular.module('openWeatherMapApp')
         cityID: $routeParams.cityID
     });
 
-    $scope.saveCity = function(city){
-    var cityData = {
-        'name': city.name,
-        'id': city.id
-    };
-    if (!$localStorage.savedCities){
-        $localStorage.savedCities = [cityData];
-    } else {
-        // We have already saved some cities. 
-        // Check to make sure we haven't already saved the current city.
-        var save = true; // Initialize the save decision variable.
-        // Use this loop to check if we've already saved the city.
-        for (var i=0; i < $localStorage.savedCities.length; i++){
-            if ($localStorage.savedCities[i].id == cityData.id) {
-                // This is a duplicate, so don't save (variable set to false).
-                save = false;
-            }
-        }
-        if (save==true){
-            $localStorage.savedCities.push(cityData);
-        } else {
-            console.log('city already saved');
-        }
-    }
-};
+
   });
